f8b680d08cc433c97ebcb5f9de1e37bf05979071
Jose Andres <Gorusuke123@gmail.com>
2022-09-09 10:32:46 -0500

    fixing conflicts

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-
-
-// export default defineConfig(({ command, mode }) => {
-//   // Load env file based on `mode` in the current working directory.
-//   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-//   const env = loadEnv(mode, process.cwd(), '')
-//   return {
-//     // vite config
-//     define: {
-//       __APP_ENV__: env.APP_ENV
-//     },
-//     plugins: [react()]
-//   }
-// })
-
 export default defineConfig({
   plugins: [react()],
 })
